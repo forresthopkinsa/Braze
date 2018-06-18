34748c4c5cb8ddaeee5fdb111d93c17a59fb29c8
Forrest Hopkins <fhopkins404@gmail.com>
2018-06-18 14:45:26 -0700

    H2 database on disk

---
+++ src/main/kotlin/com/forresthopkinsa/braze/spring/DataConfiguration.kt
@@ -0,0 +1,42 @@
+package com.forresthopkinsa.braze.spring
+
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.orm.jpa.JpaTransactionManager
+import org.springframework.orm.jpa.JpaVendorAdapter
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
+import org.springframework.orm.jpa.vendor.Database
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
+import javax.persistence.EntityManagerFactory
+import javax.sql.DataSource
+
+@Configuration
+@EnableJpaRepositories(basePackages = ["com.forresthopkinsa.braze.data"])
+class DataConfiguration {
+
+    @Bean
+    fun dataSource(): DataSource = DataSourceBuilder.create()
+            .username("sa")
+            .url("jdbc:h2:./braze;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;")
+            .build()
+
+    @Bean
+    fun jpaVendorAdapter(): JpaVendorAdapter = HibernateJpaVendorAdapter().apply {
+        setDatabase(Database.H2)
+        setGenerateDdl(true)
+    }
+
+    @Bean
+    fun entityManagerFactory(ds: DataSource, adapter: JpaVendorAdapter): LocalContainerEntityManagerFactoryBean =
+            LocalContainerEntityManagerFactoryBean().apply {
+                dataSource = ds
+                jpaVendorAdapter = adapter
+                setPackagesToScan("com.forresthopkinsa.braze.model")
+            }
+
+    @Bean
+    fun transactionManager(emf: EntityManagerFactory): JpaTransactionManager = JpaTransactionManager(emf)
+
+}
